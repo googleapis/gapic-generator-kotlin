4b66ba5cfc8fafa5e5e5f72105209f6682c12de7
Joe Bolinger <jbolinger@google.com>
2018-07-12 18:11:48 -0700

    added basic sanity tests and copyright

---
+++ examples/app/src/androidTest/java/com/google/experimental/examples/kotlin/client/MainActivityLROTest.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.experimental.examples.kotlin.client
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import com.google.experimental.examples.kotlin.R
+import org.hamcrest.CoreMatchers.containsString
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainActivityLROTest {
+
+    @Rule
+    @JvmField
+    val rule: ActivityTestRule<MainActivityLRO> =
+            ActivityTestRule(MainActivityLRO::class.java)
+
+    @Test
+    fun returnsRecognizedResult() {
+        awaitApiCall()
+
+        onView(withId(R.id.text_view))
+                .check(matches(withText(containsString("Brooklyn Bridge"))))
+    }
+
+}
---
+++ examples/app/src/androidTest/java/com/google/experimental/examples/kotlin/client/TestUtil.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.experimental.examples.kotlin.client
+
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import com.google.experimental.examples.kotlin.R
+import org.awaitility.Awaitility
+import org.hamcrest.Matchers.not
+
+// waits until the loading text goes away, useful for simple tests
+fun awaitApiCall() {
+    Awaitility.await().untilAsserted {
+        Espresso.onView(ViewMatchers.withId(R.id.text_view))
+                .check(ViewAssertions.matches(not(withText(R.string.loading))))
+    }
+}
